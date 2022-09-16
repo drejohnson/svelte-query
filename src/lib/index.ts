18afd3a6fcc9c4d6e3bbb96b0d6be077ec581cc1
DeAndre Johnson <dre@phreshr.com>
2022-09-16 10:56:18 +0800

    removed isRestoring

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -4,7 +4,6 @@ export * from '$lib/infiniteQuery/index.js';
 export * from '$lib/queries/index.js';
 export * from '$lib/isFetching/index.js';
 export * from '$lib/isMutating/index.js';
-export * from '$lib/isRestoring/index.js';
 export * from '$lib/mutation/index.js';
 export * from '$lib/hydration/index.js';
 export * from '$lib/types.js';
